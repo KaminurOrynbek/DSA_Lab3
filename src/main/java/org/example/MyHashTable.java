4141f16a3f3097b281988726671ec4f1344c6d9b
KaminurOrynbek <153558671+KaminurOrynbek@users.noreply.github.com>
2024-05-17 10:08:41 +0600

    MyHashTable.java

--- src/main/java/org/example/MyHashTable.java
+++ src/main/java/org/example/MyHashTable.java
@@ -1,24 +1,17 @@
 package org.example;
 
 public class MyHashTable<K, V> {
-    public class HashNode<K, V> {
+    private class HashNode<K, V> {
         private K key;
         private V value;
-        private HashNode<K, V> next;
+        private HashNode<K, V> next; // helps when collision occurs
 
+        // constructor of HashNode
         public HashNode(K key, V value) {
             this.key = key;
             this.value = value;
         }
-        public K getKey() {
-            return this.key;
-        }
-        public V getValue() {
-            return this.value;
-        }
-        public void setValue(V value) {
-            this.value = value;
-        }
+
         @Override
         public String toString() {
             return "{" + key + " " + value + "}";
@@ -26,129 +19,119 @@ public class MyHashTable<K, V> {
     }
 
     private HashNode<K, V>[] chainArray;
-    private int M = 11;
-    private int size;
-    public HashNode<K, V>[] getChainArray() {
-        return chainArray;
+    private int M; // default capacity M = 11
+    private int size; // number of HashNodes
+
+    public int getSizeOfArray(int i){
+        return chainArray.length;
     }
 
-    public int getSize() {
-        return size;
-    }
-
+    // default constructor of MyHashTable with no parameter
     public MyHashTable() {
-        chainArray = new HashNode[M];
+        this(11);
     }
 
+    // constructor of MyHashTable
+    // @param: int M -> user entered capacity
     public MyHashTable(int M) {
         this.M = M;
-        chainArray = new HashNode[M];
+        chainArray = new HashNode[M]; // creating new instance of HashNode by array with size M
+        this.size = 0; // at first our number of HashNode will be 0, because we don't have inserted key,value pair yet
     }
 
-    private int hash(K key) {
-        return key.toString().length() % M;
+    // my implemented hash function that returns chainArray index of our HashNode
+    // @param: K (generic type) key
+    // @return: int
+    public int hash(K key) {
+        return key.toString().length() % M; // our key's type is generic and that's why we need toString func to access length of key
     }
 
+    // insertion new key, value pair with addFirst method
+    // @param: K key, V value
+    // @return: no return type;
     public void put(K key, V value) {
-        int hash = hash(key);
-        HashNode<K, V> node = chainArray[hash];
-
-        if (node == null) {
-            chainArray[hash] = new HashNode<K, V>(key, value);
-        } else {
-            while (node.next != null) { // key exists
-                if (node.getKey() == key) {
-                    node.setValue(value);
-                    return;
-                }
-                node = node.next;
-            }
-            if (node.getKey() == key) { // key doesn't exist
-                node.setValue(value);
-                return;
-            }
-            node.next = new HashNode<K, V>(key, value);
+        if(key == null || value == null) {
+            throw new IllegalArgumentException("Key or Value is null !!!");
         }
-        size++; // Increment size after adding a new element
+
+        int chainArrayIndex = hash(key); // receiving index in our array
+        HashNode<K, V> head = chainArray[chainArrayIndex]; // head of chain of HashNodes is first node
+        while(head != null) {
+            if(head.key.equals(key)) { // if head.key equals inserted key I'm simply updating value
+                head.value = value;
+                return; // if it equals we stop our while loop
+            }
+            head = head.next; // if it is not equal we are searching head.next -> null to place our node
+        }
+        head = chainArray[chainArrayIndex]; // we are saying to our prev node be a head
+        HashNode node = new HashNode(key, value); // creating new node
+        node.next = head; // we are placing our prev node next to new node
+        chainArray[chainArrayIndex] = node; // and now our first node of chainArray is new node
+        size++; // our number of HashNodes incremented
     }
 
+    // this function gets value by key from chainArray
+    // @param: K key
+    // @return: V value
     public V get(K key) {
-        int hash = hash(key);
-        HashNode<K, V> node = chainArray[hash];
-
-        if (node == null) {
-            return null;
+        if(key == null) {
+            throw new IllegalArgumentException("Key is null !!!");
         }
-        while (node != null) {
-            if (node.getKey() == key) {
-                return node.getValue();
+
+        int chainArrayIndex = hash(key);
+        HashNode<K, V> head = chainArray[chainArrayIndex];
+        while(head != null) {
+            if(head.key.equals(key)) {
+                return head.value;
             }
-            node = node.next;
+            head = head.next;
         }
         return null;
     }
 
     public V remove(K key) {
-        int hash = hash(key);
-        HashNode<K, V> node = chainArray[hash];
+        if(key == null) {
+            throw new IllegalArgumentException("Key is null !!!");
+        }
+
+        int chainArrayIndex = hash(key);
+        HashNode<K, V> head = chainArray[chainArrayIndex];
         HashNode<K, V> prev = null;
-
-        while (node != null) {
-            if (node.getKey() == key) {
-                if (prev == null) {
-                    // If the node to be removed is the first node in the chain
-                    chainArray[hash] = node.next;
-                } else {
-                    prev.next = node.next;
-                }
-                // Return the value of the removed node
-                V removedValue = node.getValue();
-                node.next = null; // Disconnect the removed node from the chain
-                size--; // Update the size of the hash table
-                return removedValue;
+        while(head != null) {
+            if(head.key.equals(key)) {
+                break;
             }
-            prev = node;
-            node = node.next;
+            prev = head;
+            head = head.next;
         }
-        return null; // Key not found
+        if(prev != null){
+            prev.next = head.next;
+        } else {
+            chainArray[chainArrayIndex] = head.next;
+        }
+        return head.value;
     }
 
-
+    // checks whether chainArray is empty or not
+    // @param: V value
+    // @return: true or false
     public boolean contains(V value) {
-        // Iterate over each chain in the hash table
-        for (HashNode<K, V> chain : chainArray) {
-            // Iterate over each node in the current chain
-            HashNode<K, V> current = chain;
-            while (current != null) {
-                // Check if the current node's value matches the given value
-                if (current.getValue().equals(value)) {
-                    // If found, return true
-                    return true;
-                }
-                // Move to the next node in the chain
-                current = current.next;
-            }
-        }
-        // If the value is not found in any chain, return false
-        return false;
+        return size == 0;
     }
 
+    // gets key of HasNode by value
+    // @param: V value
+    // @return: K key
     public K getKey(V value) {
-        // Iterate over each chain in the hash table
         for (HashNode<K, V> chain : chainArray) {
-            // Iterate over each node in the current chain
             HashNode<K, V> current = chain;
-            while (current != null) {
-                // Check if the current node's value matches the given value
-                if (current.getValue().equals(value)) {
-                    // If found, return the key of the current node
-                    return current.getKey();
+            while(current != null) {
+                if(current.value.equals(value)) {
+                    return current.key;
                 }
-                // Move to the next node in the chain
                 current = current.next;
             }
         }
-        // If the value is not found in any chain, return null
         return null;
     }
 }
