c77b932beb4b95c7ab6a54ac820a69a2e006d503
KaminurOrynbek <153558671+KaminurOrynbek@users.noreply.github.com>
2024-05-24 01:06:31 +0600

    MyHashTableTest.java

--- src/main/java/org/example/MyHashTableTest.java
+++ src/main/java/org/example/MyHashTableTest.java
@@ -1,34 +1,33 @@
 package org.example;
-
 import java.util.Random;
+
 
 public class MyHashTableTest {
     public static void main(String[] args) {
-        MyHashTable<MyTestingClass, Student> table = new MyHashTable<>();
-        // Adding random 10000 elements to the hashtable
+        MyHashTable<Subjects, Student> hashTable = new MyHashTable<>(11);
+
         Random random = new Random();
+
         for (int i = 0; i < 10000; i++) {
-            int id = random.nextInt(10000); // Random id
-            MyTestingClass key = new MyTestingClass(id);
-            Student student = new Student("Student " + i, 20 + random.nextInt(10)); // Random student
-            table.put(key, student);
+            String subjectId = "Subject number is " + random.nextInt(10000);
+            Subjects subjects = new Subjects(subjectId);
+
+            int studentId = random.nextInt(10000);
+            String firstName = "Firstname " + random.nextInt(10000) + ",";
+            String surname = "Surname " + (random.nextInt(10000))+ ".";
+
+            Student student = new Student(studentId, firstName, surname);
+            hashTable.put(subjects, student);
+
+            System.out.println(subjects + " - student id is " + student);
+
         }
 
-        // Print number of elements in each bucket (chain or linkedlist)
-        printBucketSizes(table);
+
     }
 
-    private static void printBucketSizes(MyHashTable<MyTestingClass, Student> table) {
-        int numBuckets = table.getSize(); // Using size() method instead of accessing chainArray directly
-        for (int i = 0; i < numBuckets; i++) {
-            int count = 0;
-            MyHashTable<MyTestingClass, Student>.HashNode<MyTestingClass, Student> node = table.getChainArray()[i]; // Using getChainArray() method
-            while (node != null) {
-                count++;
-
-            }
-            System.out.println("Bucket " + i + ": " + count);
-        }
     }
 
-}
+
+
+
